bd010fe74df2de25564b21014e03486e26ff5155
npkk <kob4.nemu@gmail.com>
2025-08-11 10:31:10 +0900

    feat: fix pg_dump script

--- tools/postgres_dump.ps1
+++ tools/postgres_dump.ps1
@@ -16,7 +16,7 @@ $user = Get-Content -Path $userFile -Raw
 $password = Get-Content -Path $passwordFile -Raw
 $dbName = Get-Content -Path $dbNameFile -Raw
 
-docker compose exec -e PGPASSWORD=$password db pg_dump -U $user -d $dbName -F c --data-only > $dumpFile
+docker compose exec -e PGPASSWORD=$password db pg_dump -U $user -d $dbName -F c > $dumpFile
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Database dump completed successfully."
