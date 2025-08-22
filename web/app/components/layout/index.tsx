8dc0dde554c638b3fa3410f345b3e4816e6c3b4b
npkk <kob4.nemu@gmail.com>
2025-08-22 12:54:19 +0900

    refactor: lint
    - lint対応
    - precommitか何か仕込んだ方が良いかもしれない

--- web/app/components/layout/index.tsx
+++ web/app/components/layout/index.tsx
@@ -1,13 +1,13 @@
-import type React from 'react';
+import type React from "react";
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className="p-4 bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 min-h-screen">
-      {children}
-    </div>
-  );
+	return (
+		<div className="p-4 bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 min-h-screen">
+			{children}
+		</div>
+	);
 };
