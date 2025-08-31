5f97c7ff896485344d02cf439b4b2326fcc03c40
npkk <kob4.nemu@gmail.com>
2025-08-31 19:10:13 +0900

    feat: fix dump script
    - 内部に吐いてからdocker cpするように修正

--- tools/postgres_dump.ps1
+++ tools/postgres_dump.ps1
@@ -7,7 +7,8 @@ $dbNameFile = Join-Path -Path $secretsDir -ChildPath "postgres_db"
 $dumpDir = Resolve-Path (Join-Path -Path $scriptDir -ChildPath "..\dump")
 
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$dumpFile = Join-Path -Path $dumpDir -ChildPath "dump_${timestamp}.dump"
+$dumpFileName = "dump_${timestamp}.dump"
+$dumpFile = Join-Path -Path $dumpDir -ChildPath $dumpFileName
 
 Write-Host "Starting database dump..."
 Write-Host "Output file: ${dumpFile}"
@@ -16,7 +17,8 @@ $user = Get-Content -Path $userFile -Raw
 $password = Get-Content -Path $passwordFile -Raw
 $dbName = Get-Content -Path $dbNameFile -Raw
 
-docker compose exec -e PGPASSWORD=$password db pg_dump -U $user -d $dbName -F c > $dumpFile
+docker compose exec -e PGPASSWORD=$password db pg_dump -U $user -d $dbName -F c -f /tmp/$dumpFileName
+docker compose cp db:/tmp/$dumpFileName $dumpFile
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Database dump completed successfully."
