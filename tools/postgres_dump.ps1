218be0deef1e256634b2add4ae26865936d20647
npkk <koba.dev.npkk@gmail.com>
2025-10-07 14:53:26 +0900

    fix: postgres dump script

--- tools/postgres_dump.ps1
+++ tools/postgres_dump.ps1
@@ -9,6 +9,7 @@ $dumpDir = Resolve-Path (Join-Path -Path $scriptDir -ChildPath "..\dump")
 $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
 $dumpFileName = "dump_${timestamp}.dump"
 $dumpFile = Join-Path -Path $dumpDir -ChildPath $dumpFileName
+$serviceName = "kokkai-summary-db"
 
 Write-Host "Starting database dump..."
 Write-Host "Output file: ${dumpFile}"
@@ -17,9 +18,9 @@ $user = Get-Content -Path $userFile -Raw
 $password = Get-Content -Path $passwordFile -Raw
 $dbName = Get-Content -Path $dbNameFile -Raw
 
-docker compose exec -e PGPASSWORD=$password db pg_dump -U $user -d $dbName -F c -f /tmp/$dumpFileName
-docker compose cp db:/tmp/$dumpFileName $dumpFile
-docker compose exec db rm /tmp/$dumpFileName
+docker exec -e PGPASSWORD=$password $serviceName pg_dump -U $user -d $dbName -F c -f /tmp/$dumpFileName
+docker cp ${serviceName}:/tmp/$dumpFileName $dumpFile
+docker exec $serviceName rm /tmp/$dumpFileName
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Database dump completed successfully."
